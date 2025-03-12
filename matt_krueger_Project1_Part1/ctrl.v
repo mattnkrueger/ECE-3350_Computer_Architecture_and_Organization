7194840f6b0779a7448618599f7999db372857b5
mattnkrueger <mkrue138@gmail.com>
2025-03-11 23:06:33 -0500

    corrected code

--- matt_krueger_Project1_Part1/ctrl.v
+++ matt_krueger_Project1_Part1/ctrl.v
@@ -1,4 +1,4 @@
-// ECE:3350 SISC processor project
+// ECE:3350 SISC computer project
 // finite state machine
 
 `timescale 1ns/100ps
@@ -9,151 +9,107 @@ module ctrl (clk, rst_f, opcode, mm, stat, rf_we, alu_op, wb_sel);
   output reg rf_we, wb_sel;
   output reg [3:0] alu_op;
   
-  // state parameter declarations
+  // states
   parameter start0 = 0, start1 = 1, fetch = 2, decode = 3, execute = 4, mem = 5, writeback = 6;
    
-  // opcode parameter declarations
+  // opcodes
   parameter NOOP = 0, REG_OP = 1, REG_IM = 2, SWAP = 3, BRA = 4, BRR = 5, BNE = 6, BNR = 7;
   parameter JPA = 8, JPR = 9, LOD = 10, STR = 11, CALL = 12, RET = 13, HLT = 15;
 	
   // addressing modes
-  parameter AM_IMM = 8;
+  // parameter AM_IMM = 8;
 
-  // state register and next state signal
+  // state registers
   reg [2:0]  present_state, next_state;
-  
-  // initial procedure to initialize the present state to 'start0'.
+
+  initial
+    present_state = start0;
+
   always @(posedge clk, negedge rst_f)
-    begin
-      if (rst_f == 1'b0)
-        present_state <= start1;
-      else
-        present_state <= next_state;
-    end
-  
-  /* The following combinational procedure determines the next state of the fsm. */
+  begin
+    if (rst_f == 1'b0)
+      present_state <= start1;
+    else
+      present_state <= next_state;
+  end
 
   always @(present_state, rst_f)
-    begin
-      case(present_state)
-        start0:
-          next_state = start1;
-        start1:
-      if (rst_f == 1'b0) 
-          next_state = start1;
-    else
-          next_state = fetch;
-        fetch:
-          next_state = decode;
-        decode:
-          next_state = execute;
-        execute:
-          next_state = mem;
-        mem:
-          next_state = writeback;
-        writeback:
-          next_state = fetch;
-        default:
-          next_state = start1;
-      endcase
+  begin
+    case(present_state)
+      start0:
+        next_state = start1;
+      start1:
+	  if (rst_f == 1'b0) 
+        next_state = start1;
+	 else
+         next_state = fetch;
+      fetch:
+        next_state = decode;
+      decode:
+        next_state = execute;
+      execute:
+        next_state = mem;
+      mem:
+        next_state = writeback;
+      writeback:
+        next_state = fetch;
+      default:
+        next_state = start1;
+    endcase
+  end
+
+  // Halt on HLT instruction
+  always @ (opcode)
+  begin
+    if (opcode == HLT)
+    begin 
+      #5 $display ("Halt."); //Delay 5 ns so $monitor will print the halt instruction
+      $stop;
     end
+  end
+    
+  /* TODO: Generate outputs based on the FSM states and inputs. For Parts 2 and 3, you will
+       add the new control signals here. */
 
   always @(present_state, opcode)
-    begin
-      // default values for control signals
-      rf_we = 1'b0;    // disabled write option
-      wb_sel = 1'b0;   // mux select default to alu writeback
-      alu_op = 4'b0000; // NOP
-      
-      case(present_state)
-      // if in early stages of fsm -> 
-      // register file write still disabled (need to read)
-      // write back no
-      // no operation by alu
-        start0, start1, fetch:
-          begin
-            rf_we = 1'b0; 
-            wb_sel = 1'b0; 
-            alu_op = 4'b0000;
-          end
-          
-        decode:
-          begin
+  begin
 
-          // no changes to write enable and mux select (need to read)
-            rf_we = 1'b0;
-            wb_sel = 1'b0;
-            
-            case(opcode)
-              // opcode mapping for wanted signals (via Part 1.pdf)
-              NOP:    alu_op = NOP;
-              ADD:    alu_op = ADD;
-              ADI:    alu_op = ADI;
-              SUB:    alu_op = SUB;
-              NOT:    alu_op = NOT;
-              OR:     alu_op = OR;
-              AND:    alu_op = AND; // this is updating the operation code sent to the ALU mapping current value to wanted value...
-              XOR:    alu_op = XOR; // essentially just passing what the current opcode is as we already know constants parameters representing the opcode
-              ROR:    alu_op = ROR;
-              ROL:    alu_op = ROL;
-              SHR:    alu_op = SHR;
-              SHL:    alu_op = SHL;
-              HLT:    alu_op = HLT;
-              default: alu_op = 4'b0000; // default to NOP
-            endcase
-          end
-          
-        execute:
-          // ALU execution stage
-          begin
-            rf_we = 1'b0;
-            
-            if (opcode == REG_IM)
-              wb_sel = 1'b1; // write back eneabled -> mux to output alu result
-            else
-              wb_sel = 1'b0;
-          end
-          
-        mem:
-          // keep we disabled
-          begin
-            rf_we = 1'b0;
-          end
-          
-        writeback:
-        // write alu result into the RF
-          begin
-            case(opcode)
-              REG_OP, REG_IM:
-                rf_we = 1'b1; 
-              SWAP:
-                rf_we = 1'b1; 
-              default:
-                rf_we = 1'b0;
-            endcase
-          end
-          
-        default:
-        // default values 
-        // register file write still disabled
-        // write back no
-        // no operation by alu
-          begin
-            rf_we = 1'b0;
-            wb_sel = 1'b0;
-            alu_op = 4'b0000;
-          end
-      endcase
-    end
+    rf_we  = 1'b0;
+    wb_sel = 1'b0;
+    alu_op = 4'b0000;
+        
+    case(present_state)
 
-// Halt on HLT instruction
-  always @ (opcode)
-    begin
-      if (opcode == HLT)
-      begin 
-        #5 $display ("Halt."); //Delay 5 ns so $monitor will print the halt instruction
-        $stop;
+      execute:
+      begin
+        if (opcode == REG_OP)
+          alu_op = 4'b0001;
+        if (opcode == REG_IM)
+          alu_op = 4'b0011;
       end
-    end
-  
+
+      mem:
+      begin
+        if (opcode == REG_OP)
+          alu_op = 4'b0000;
+        if (opcode == REG_IM)
+          alu_op = 4'b0010;
+      end
+
+      writeback:
+      begin
+        if (opcode == REG_OP || opcode == REG_IM)
+          rf_we  = 1'b1;
+      end
+
+      default:
+      begin
+        rf_we  = 1'b0;
+        wb_sel = 1'b0;
+        alu_op = 4'b0000;
+      end
+      
+    endcase
+  end
+
 endmodule
--- matt_krueger_Project1_Part1/sisc.v
+++ matt_krueger_Project1_Part1/sisc.v
@@ -4,122 +4,32 @@
 `timescale 1ns/100ps  
 
 module sisc (clk, rst_f, ir);
-    // Inputs: 
-    //
-    // 1-Bit Inputs:
-    //  clk - clock signal
-    //  rst_f - reset signal
-    //
-    // 32-Bit Inputs:
-    //  ir - Instruction Register
-    input clk, rst_f;
-    input [31:0] ir;
 
-    // Outputs:
-    //  None
-    // Wires: exhaustive list of wires on the diagram
-    //  ir[31:28] of instruction[31:0] -> CTRL
-    //  ir[27:24] of instruction[31:0] -> CTRL
-    //  ir[19:16] of instruction[31:0] -> RF
-    //  ir[23:20] of instruction[31:0] -> RF
-    //  ir[15:12] of instruction[31:0] -> RF
-    //  ir[15:0] of instruction[31:0] -> ALU
-    //  CLK (SISC_tb) -> CTRL, RF, STATREG, & ALU
-    //  RST_F (SISC_tb) -> CTRL 
-    //  RF output A -> ALU
-    //  RF output B -> ALU
-    //  CTRL -> RF
-    //  ALU Control -> STATREG
-    //  ALU CC -> STATREG 
-    //  ALU Out -> MUX32 0
-    //  0 -> MUX32 1
-    //  MUX32 -> RF
-    //  CTRL -> ALU
-    //  CTRL -> MUX32
-    wire [31:0] mux_out;    // mux out
-    wire [31:0] rsa;        // rf out
-    wire [31:0] rsb;        // rf out
-    wire [31:0] alu_result; // alu out 32-bit
-    wire [3:0] stat;        // alu status output
-    wire [3:0] stat_en;     // alu status enable
-    wire [3:0] stat_out;    // status reg out
-    wire rf_we;             // register file write enable from control
-    wire [3:0] alu_op;      // ALU operation from control
-    wire wb_sel;            // write back select from control
+  input clk, rst_f;
+  input [31:0] ir;
 
-    // Control Unit Instantiation (CTRL)
-    // 
-    // Signature:
-    //  module ctrl (clk, rst_f, opcode, mm, stat, rf_we, alu_op, wb_sel);
-    ctrl sisc_ctrl (
-        .clk(clk),
-        .rst_f(rst_f),
-        .opcode(ir[31:28]),
-        .mm(ir[27:24]), 
-        .stat(stat_out),
-        .rf_we(rf_we),              // output 
-        .alu_op(alu_op),            // output
-        .wb_sel(wb_sel)             // output
-    );
-    
-    // Register File Instantiation (RF)
-    //
-    // Signature:
-    //  module rf (clk, read_rega, read_regb, write_reg, write_data, rf_we, rsa, rsb);
-    rf sisc_rf (
-        .clk(clk),
-        .read_rega(ir[19:16]),         
-        .read_regb(ir[15:12]),    
-        .write_reg(ir[23:20]),   
-        .write_data(mux_out),      
-        .rf_we(rf_we),              
-        .rsa(rsa),                  // output 
-        .rsb(rsb)                   // output 
-    );
+  wire rf_we, wb_sel, c_in;
+  wire [3:0] alu_op;
+  wire [3:0] alu_sts, stat, stat_en;
+  wire [31:0] rega, regb, wr_dat, alu_out;
+  
 
-    // Status Register Instantiation (STAT)
-    //
-    // Signature:
-    //  module statreg (clk, in, enable, out);
-    statreg sisc_statreg (
-        .clk(clk),
-        .in(stat),
-        .enable(stat_en),
-        .out(stat_out)             // output
-    );
+// component instantiation
 
-    // Arithmetic Logic Unit Instantiation (ALU)
-    //
-    // Signature:
-    //  module alu (clk, rsa, rsb, imm, c_in, alu_op, funct, alu_result, stat, stat_en);
-    alu sisc_alu (
-        .clk(clk),
-        .rsa(rsa),
-        .rsb(rsb),
-        .imm(ir[15:0]),
-        .c_in(1'b0),              // 0 as carry-in
-        .alu_op(alu_op),
-        .alu_result(alu_result),  // output
-        .stat(stat),              // output
-        .stat_en(stat_en)         // output
-    );
+  ctrl u1 (clk, rst_f, ir[31:28], ir[27:24], stat, rf_we, alu_op, wb_sel);
 
-    // Multiplexor (MUX32)
-    //
-    // Signature:
-    //  module mux32 (in_a, in_b, sel, out);
-    mux32 sisc_mux32 (
-        .in_a(alu_result),
-        .in_b(32'b0),
-        .sel(wb_sel),
-        .out(mux_out)             // output
-    );
+  rf u2 (clk, ir[19:16], ir[15:12], ir[23:20], wr_dat, rf_we, rega, regb);
 
-    // Monitoring the required signals - (via Part 1.pdf)
-    initial begin
-        $monitor("Time: %0t, IR=%h, R1=%h, R2=%h, R3=%h, R4=%h, R5=%h, ALU_OP=%h, WB_SEL=%b, RF_WE=%b, Write_Data=%h", 
-                $time, ir, sisc_rf.ram_array[1], sisc_rf.ram_array[2], sisc_rf.ram_array[3], 
-                sisc_rf.ram_array[4], sisc_rf.ram_array[5], alu_op, wb_sel, rf_we, mux_out);
-    end
+  alu u3 (clk, rega, regb, ir[15:0], stat[3], alu_op, ir[27:24], alu_out, alu_sts, stat_en);
+
+  mux32 u5 (alu_out, 32'h00000000, wb_sel, wr_dat);
+
+  statreg u6(clk, alu_sts, stat_en, stat);
+
+  initial
+  $monitor($time,,"%h  %h  %h  %h  %h  %h  %h  %b  %b  %h",ir,u2.ram_array[1],u2.ram_array[2],u2.ram_array[3],u2.ram_array[4],u2.ram_array[5],alu_op,wb_sel,rf_we,wr_dat);
+
 
 endmodule
+
+
